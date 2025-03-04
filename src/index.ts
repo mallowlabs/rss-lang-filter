6d45285d62d83e89c8c2964532afbfa75c164c42
mallowlabs <mallowlabs@gmail.com>
2025-03-04 19:52:57 +0900

    Add /feed

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+const FEED_URL = 'https://dev.classmethod.jp/feed/';
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/feed', async (c) => {
+  const response = await fetch(FEED_URL);
+  const data = await response.text();
+  return c.text(data);
 })
 
 export default app
