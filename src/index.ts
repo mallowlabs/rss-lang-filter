4f4f0637d9672745e30a2fe1d30c738104b20e8d
mallowlabs <mallowlabs@gmail.com>
2025-03-04 20:36:36 +0900

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,21 @@ const deserializer = new DOMParser();
 
 const FEED_URL = 'https://dev.classmethod.jp/feed/';
 
+const filterFeed = (text: string, language: string) => {
+  const doc = deserializer.parseFromString(text, 'text/xml');
+  const channel = doc.getElementsByTagName('channel')[0];
+  const items = channel.getElementsByTagName('item');
+  const deletingItems = Array.from(items).filter((item) => {
+    const title = item.getElementsByTagName('title')[0]?.textContent || ''
+    const description = item.getElementsByTagName('description')[0]?.textContent || '';
+
+    const lang = eld.detect(title + description).language;
+    return lang !== language;
+  });
+  deletingItems.forEach((item) => channel.removeChild(item));
+  return serializer.serializeToString(doc);
+}
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
@@ -16,22 +31,7 @@ app.get('/', (c) => {
 app.get('/feed', async (c) => {
   const response = await fetch(FEED_URL);
   const data = await response.text();
-
-  const doc = deserializer.parseFromString(data, 'text/xml');
-
-  const channel = doc.getElementsByTagName('channel')[0];
-
-  const items = channel.getElementsByTagName('item');
-  const deletingItems = Array.from(items).filter((item) => {
-    const title = item.getElementsByTagName('title')[0]?.textContent || ''
-    const description = item.getElementsByTagName('description')[0]?.textContent || '';
-
-    const language = eld.detect(title + description).language;
-    return language !== 'ja';
-  });
-  deletingItems.forEach((item) => channel.removeChild(item));
-
-  const text = serializer.serializeToString(doc);
+  const text = filterFeed(data, 'ja');
   return c.text(text);
 })
 
