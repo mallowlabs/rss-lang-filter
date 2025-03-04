c44533243f4d4a3173f37f724e47230051d29b0b
mallowlabs <mallowlabs@gmail.com>
2025-03-04 20:08:46 +0900

    Add filter

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Hono } from 'hono'
+import { DOMParser, XMLSerializer } from 'xmldom';
 
 const app = new Hono()
+
+const serializer = new XMLSerializer();
+const deserializer = new DOMParser();
 
 const FEED_URL = 'https://dev.classmethod.jp/feed/';
 
@@ -11,7 +15,20 @@ app.get('/', (c) => {
 app.get('/feed', async (c) => {
   const response = await fetch(FEED_URL);
   const data = await response.text();
-  return c.text(data);
+
+  const doc = deserializer.parseFromString(data, 'text/xml');
+
+  const channel = doc.getElementsByTagName('channel')[0];
+
+  const items = channel.getElementsByTagName('item');
+  const deletingItems = Array.from(items).filter((item, index) => {
+    const title = item.getElementsByTagName('title')[0];
+    return title.textContent?.includes('[Update]');
+  });
+  deletingItems.forEach((item) => channel.removeChild(item));
+
+  const text = serializer.serializeToString(doc);
+  return c.text(text);
 })
 
 export default app
