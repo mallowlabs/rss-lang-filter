d55f81f4df528041bed6c0cbeeda7237da78bc46
mallowlabs <mallowlabs@gmail.com>
2025-03-04 20:32:44 +0900

    Add language filter

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { DOMParser, XMLSerializer } from '@xmldom/xmldom';
+import { eld } from 'efficient-language-detector-no-dynamic-import';
 
 const app = new Hono()
 
@@ -21,9 +22,12 @@ app.get('/feed', async (c) => {
   const channel = doc.getElementsByTagName('channel')[0];
 
   const items = channel.getElementsByTagName('item');
-  const deletingItems = Array.from(items).filter((item, index) => {
-    const title = item.getElementsByTagName('title')[0];
-    return title.textContent?.includes('[Update]');
+  const deletingItems = Array.from(items).filter((item) => {
+    const title = item.getElementsByTagName('title')[0]?.textContent || ''
+    const description = item.getElementsByTagName('description')[0]?.textContent || '';
+
+    const language = eld.detect(title + description).language;
+    return language !== 'ja';
   });
   deletingItems.forEach((item) => channel.removeChild(item));
 
